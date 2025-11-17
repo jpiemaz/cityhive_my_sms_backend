e51db13ea6a5e7720e96feb450a4ed9609879e49
John Mazzoli <6391200+jpiemaz@users.noreply.github.com>
2025-11-17 02:25:51 -0500

    Add endpoint to create messages

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -9,6 +9,21 @@ module Api
         messages = current_user.messages.all
         render json: messages, status: :ok
       end
+
+      def create
+        message = current_user.messages.new(message_params)
+        if message.save!
+          render json: message, status: :created
+        else
+          render json: { errors: message.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def message_params
+        params.require(:message).permit(:text, :phone_number)
+      end
     end
   end
 end
